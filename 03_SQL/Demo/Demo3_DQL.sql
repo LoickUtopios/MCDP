a1e436eca009819c27db04a3c8ccc766b3539f06
Loick <loick@utopios.solutions>
2026-01-14 09:52:51 +0100

    Ajout des exercices 10 a 12 + demo 3

--- 03_SQL/Demo/Demo3_DQL.sql
+++ 03_SQL/Demo/Demo3_DQL.sql
@@ -38,3 +38,68 @@ WHERE salaire > 3000 OR (age < 30 AND NOT service_id = 1);
 SELECT DISTINCT service_id 
 FROM salarie; 
 
+-- Nous pouvons fournir une plage de valeur dans notre filtre WHERE
+-- Pour rechercher des termes spécifiques, nous utilisons le mot-clé IN
+SELECT * 
+FROM salarie
+WHERE prenom IN ('Eren', 'Walter', 'Jasquier');  
+
+SELECT * 
+FROM salarie
+WHERE age NOT IN (20, 25, 30); 
+
+-- Pour rechercher un élément qui fait partie d'une plage de valeur, nous utilisons BETWEEN..AND...
+SELECT *
+FROM salarie
+WHERE salaire BETWEEN 1500 AND 2500; -- Les valeurs données sont incluses.
+
+SELECT *
+FROM salarie
+WHERE age NOT BETWEEN 20 AND 30;
+
+-- Le LIKE permet d'effectuer une recherche sur une partie d'une chaine de caractere
+SELECT *
+FROM salarie
+WHERE prenom LIKE 'J%';
+
+SELECT *
+FROM salarie
+WHERE prenom LIKE 'J___';
+
+SELECT *
+FROM salarie
+WHERE prenom LIKE '%g%';
+
+-- ORDER BY
+-- Permet d'ordonner le jeu de résultat par rapport à une ou plusieurs colonnes
+-- de manière croissante (ASC) ou décroissante (DESC)
+-- Par défaut: ASC, pas besoin de le préciser
+SELECT * 
+FROM salarie
+ORDER BY prenom; 
+
+SELECT * 
+FROM salarie
+ORDER BY age DESC; 
+
+SELECT * 
+FROM salarie
+ORDER BY service_id, prenom; 
+
+-- LIMIT 
+-- Permet de limiter le nombre de résultat obtenue
+SELECT prenom, salaire
+FROM salarie
+ORDER BY salaire DESC
+LIMIT 3;
+
+-- OFFSET permet de décaler le jeu de résultat que l'on récupère
+SELECT prenom, salaire
+FROM salarie
+ORDER BY salaire DESC
+LIMIT 3 OFFSET 3;
+
+
+
+
+
