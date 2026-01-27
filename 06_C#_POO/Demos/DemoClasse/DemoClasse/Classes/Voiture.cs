384e5db18a9dbbd51e62faf19396a6b704ef122e
Loick <loick@utopios.solutions>
2026-01-27 11:34:02 +0100

    Ajout de la demo1 + exo 1 a 3

---
+++ 06_C#_POO/Demos/DemoClasse/DemoClasse/Classes/Voiture.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoClasse.Classes
+{
+    internal class Voiture
+    {
+        // Attributs
+        public int _nbPneu; // Attribut publique (accès à tous)
+        private int _nbPorte; // Attibut privé (accessible uniquement dans la classe)
+        private string _marque;
+        private string _model;
+
+        // Propriétés
+        // Get permet la récupération de donnée
+        // Set est uniqument pour la modification de donnée
+        public int NbPorte { get => _nbPorte; set => _nbPorte = value; }
+        public string Marque { get => _marque; set => _marque = value; }
+        public string Model
+        {
+            get
+            {
+                return _model;
+            }
+            set
+            {
+                _model = value;
+            }
+        }
+        public bool IsStarted { get; set; } = false;
+
+        // Constructeur
+        public Voiture() { }
+        public Voiture(int nbPneu, int nbPorte, string marque, string model)
+        {
+            _nbPneu = nbPneu; // Attribut accéder directement (public)
+            NbPorte = nbPorte; // Attribut accéder via leur propriété
+            Marque = marque;
+            Model = model;
+        }
+
+        // Méthodes (fonctions de la classe)
+        public void StartEngine()
+        {
+            if (!IsStarted)
+            {
+                Console.WriteLine($"La voiture {Model} démarre !!! ");
+                IsStarted = true;
+            }
+            else
+            {
+                Console.WriteLine("Crrrrrrrr la voiture est déjà démarré !!!!");
+            }
+            
+        }
+
+        public override string ToString()
+        {
+            return $"Je suis une voiture de marque {Marque} et de model {Model}. J'ai {_nbPneu} pneus et {NbPorte} portes.";
+        }
+    }
+}
---
+++ 06_C#_POO/Demos/DemoClasse/DemoClasse/Program.cs
@@ -0,0 +1,24 @@
+﻿using DemoClasse.Classes;
+
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Voiture v1 = new Voiture(4, 5, "Mercedes", "Classe 1"); // Appel le constructeur correspondant
+            v1.Model = "ClassA"; // On utilise le set de la propriété
+
+            Console.WriteLine(v1.Model); // On utilise le get de la propriété
+            Console.WriteLine(v1._nbPneu); // On affiche directement l'attibut (possible car elle est public)
+
+            v1.StartEngine();
+            v1.StartEngine();
+
+            Voiture v2 = new Voiture(4, 3, "Chevrolet", "Aveo");
+            v2.StartEngine();
+
+            Console.WriteLine(v2);
+        }
+    }
+}
